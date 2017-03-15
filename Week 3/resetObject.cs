62391f0b949d5b6c7e7e1197dac22a8a5ae1612e
Jacob Bashista <dev@tgb20.com>
2017-03-15 18:27:46 -0400

    Reset Game Script

---
+++ Week 3/resetObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class resetObject : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D col){
+
+		if (col.collider.tag == "Player") {
+			Application.LoadLevel (Application.loadedLevelName);
+		}
+
+
+	}
+
+
+
+}
